1aab241234a190ba6d100647f15d37fe28e0f2a2
BarbonFisso <barbieri.mat@hotmail.it>
2022-02-15 13:22:43 +0100

    Add: postcard route

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Auth::routes();
-
-// Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/', 'GuestController@home') -> name('home');
+
+// register/login
+
+Route::post('/login', 'Auth\LoginController@login')->name('login');
+Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
+// postcard
+
+Route::middleware('auth')
+        -> name('postcard.')
+        -> prefix('postcard')
+        -> group(function() {
+            Route::get('/create', 'PostcardController@create') ->name ('create');
+            Route::post('/store', 'PostcardController@store')->name('store');
+});
